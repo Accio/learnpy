e1e55b91951603280f2266c7fe39f70260a36e85
Jitao David Zhang <jitao_david.zhang@roche.com>
2021-01-05 15:36:23 +0100

    add an ill subtract

--- pytest-example/example.py
+++ pytest-example/example.py
@@ -5,3 +5,12 @@ def add(a, b):
 def test_add():
     assert add(2, 3) == 5
     assert add('Zeit', 'geist') == 'Zeitgeist'
+
+
+def subtract(a, b):
+    return a+b
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+
